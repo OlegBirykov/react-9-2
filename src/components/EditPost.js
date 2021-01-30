1f42a5f0402e34c506a6f647dc7002463cf1bca9
OlegBirykov <p_petro75@mail.ru>
2021-01-30 12:17:19 +0300

    Заглушены предупреждения компилятора

--- src/components/EditPost.js
+++ src/components/EditPost.js
@@ -1,7 +1,7 @@
-import { useContext } from 'react';
-import { Link, Redirect } from 'react-router-dom';
-import AppContext from '../AppContext';
-import request from '../utils/http';
+//import { useContext } from 'react';
+import { Link/*, Redirect */} from 'react-router-dom';
+//import AppContext from '../AppContext';
+//import request from '../utils/http';
 import editpost from '../img/editpost.jpg';
 
 function EditPost() {
